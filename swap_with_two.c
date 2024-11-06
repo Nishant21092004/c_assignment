4cb364f2d38778528038693c490e8f50ada6d2d1
nishant <nishantrewariya61@gmail.com>
2024-11-06 09:50:03 +0530

    modify swap_with_two

--- swap_with_two.c
+++ swap_with_two.c
@@ -2,17 +2,12 @@
  // made by nishant
 #include<stdio.h>
 int main(){
-    int a,b;// declaring variables
-    printf("enter the first variable a(integer only)");
-    scanf("%d",&a); //taking first avriable from user
-    printf("enter the second variable a(integer only)");
-    scanf("%d",&b); // taking second variable
-    // we will use bitwise xor operator to swap the variables
-    a=a^b; // first we take the bitwise xor of both numbers
-    // if use take bitwise (a xor b) and again perform bitwise
-    // xor operation with rest and one of the variable we will get the other variable
-    b=a^b; // here b ^(a^b) will give the value of a
-    a=a^b; // here a^(a^b) will give the value of b
-    printf("value of variable a after swaping,%d",a);
-    printf("value of variable b after swaping %d",b);
+    int a,b;
+    printf("enter both number");
+    scanf("%d %d",&a,&b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("%d %d",a,b );
+    return 0;
 }
